4a81ebe04067e8c1bb29278588f262c2c9a28219
jirka <jiri.tuzil@safecontrol.cz>
2017-05-27 21:48:56 +0200

    inicial commit

--- ServerApp/app.js
+++ ServerApp/app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+console.log("running");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
